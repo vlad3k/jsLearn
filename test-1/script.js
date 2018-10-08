3bd09c8d768c9187dcded34c557a0beaa6adf0cb
vlad3k <vlad3k@bk.ru>
2018-10-09 01:39:25 +0300

    Added new tasks and solutions

--- test-1/script.js
+++ test-1/script.js
@@ -4,8 +4,11 @@ function getRandom() {
 }
 
 // task 2
-function isNumSameFromAllSides(num) {  //Если число 17+ знаков начинает глючить
-	var arrNum = String(num).split('');
+function isNumSameFromAllSides(num) {  
+
+	return String(num) === String(num).split('').reverse('').join('');
+
+	/*var arrNum = String(num).split('');
 	for(var i = 1; i <= arrNum.length; i += 1) {
 		if(arrNum[i - 1] == arrNum[arrNum.length - i]) {
 			if(i == arrNum.length) {
@@ -15,15 +18,13 @@ function isNumSameFromAllSides(num) {  //Если число 17+ знаков н
 		} else {
 			return false;
 		}
-	} 
+	} */
 }
 
 // task 3
 function exchangeVar(a, b) {
 	console.log('Переменная a = ' + a + ', Переменная b = ' + b);
-	a = a + b;
-	b = a - b;
-	a = a - b;
+	a = [b, b=a][0];
 	return console.log('Переменная a = ' + a + ', Переменная b = ' + b);
 }
 
@@ -42,3 +43,77 @@ function countSquare(a, b, c) {
 	}
 }
 
+// task 5
+function echangeDiagonalOfMatrix() {
+	var matrix = [
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 9]
+	];
+}
+
+// task 6
+function simpleNums() {
+	var nums = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	var simpleNums = [];
+	for(var i = 0; i < nums.length; i += 1) {
+	  if(nums[i] % 2 == 0) {
+		continue;
+	  } else {
+			simpleNums.push(nums[i]);
+	  }
+	}
+	return simpleNums.length;
+}
+
+// task 7
+function sumBetweenMinMaxNums() {
+	var arr = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	var result = 0;
+	for(var i = 1; i < arr.length - 1; i += 1) {
+		result += arr[i];
+	}
+	return result;
+}
+
+// task 8
+function mathTwoNums() {
+
+	do {
+		var num1 = prompt('Введите первое число:', '');
+		var num2 = prompt('Введите второе число:', '');
+		
+		do {
+			var oper = prompt('Введите операцию либо "0" для выхода:', '');
+			switch (oper) {
+			case '+':
+				alert( parseFloat(num1) + parseFloat(num2) );
+				break;
+			case '-':
+				alert( parseFloat(num1) - parseFloat(num2) );
+				break;
+			case '/':
+				if(num2 == 0) {
+				alert('На ноль делить нельзя');
+				break
+				} else {
+				alert( parseFloat(num1) / parseFloat(num2) );
+				break;
+				}
+			case '*':
+				alert( parseFloat(num1) + ' * ' +  parseFloat(num2) + ' = ' + parseFloat(num1) * parseFloat(num2) );
+				break;
+			case '0':
+				alert('Вы вышли из программы');
+				break;
+			default:
+				alert('Нет такой операции');
+			}
+		}
+		while(oper != '+' && oper != '-' && oper != '*' && oper != '/' && oper != '0');
+		}
+		while(oper != '0');
+
+	
+	
+}
